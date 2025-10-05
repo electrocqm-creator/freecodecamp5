6796c84da005e7507c991008365b58d52e9ba475
electrocqm-creator <electrocqm@gmail.com>
2025-10-05 00:03:46 -0300

    constante path

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 console.log("hello world");
 let express = require('express');
 let app = express();
+app.use("/public", express.static(__dirname + "/public"));
 const path = require("path");
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/views/index.html"));
 });
+
 
 
 
